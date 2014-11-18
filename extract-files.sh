f0b878fb009dcd99b60758a4e32cc958eed4283a
C457 <android.c357@gmail.com>
2014-11-17 23:21:48 -0600

    aries-common: Update vendor blob extraction
    * This was outdated a long time ago
    
    Change-Id: I3446a3d3263308ae45cea580066a50d17f716057

--- extract-files.sh
+++ extract-files.sh
@@ -1,17 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 VENDOR=samsung
 DEVICE=aries-common
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
 
-echo "Pulling common files..."
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
+    OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+    FILE=${PARSING_ARRAY[0]}
+    DEST=${PARSING_ARRAY[1]}
+    if [ -z $DEST ]
+    then
+        DEST=$FILE
+    fi
     DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR
     fi
-    adb pull /system/$FILE $BASE/$FILE
+    adb pull /system/$FILE $BASE/$DEST
+    # if file does not exist try destination
+    if [ "$?" != "0" ]
+    then
+        adb pull /system/$DEST $BASE/$DEST
+    fi
 done
 
 ./setup-makefiles.sh
